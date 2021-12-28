f9e99421606e397a750992c7e6ba623cbc95e337
ChatCode <chatcode@MacBook-Pro-de-ChatCode.local>
2021-12-28 13:06:40 +0100

    ?

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userId = decodedToken.userId;
-        if (req.body.id_users && req.body.id_users !== userId) {
-            throw 'Invalid user ID';
-        } else {
-            next();
-        }
-    } catch {
-        res.status(401).json({
-            error: new Error('Invalid request!')
-        });
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const userId = decodedToken.userId;
+    if (req.body.id_users && req.body.id_users !== userId) {
+      throw "Invalid user ID";
+    } else {
+      next();
     }
+  } catch {
+    res.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
 };
